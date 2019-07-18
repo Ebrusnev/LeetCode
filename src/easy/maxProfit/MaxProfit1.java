ea95cc99186cf9c7f885bff10574591b7b430dd8
Evgenii Brusnev <evgenii.brusnev@gmail.com>
2019-07-17 21:05:25 -0500

    Max Profit Added

---
+++ src/easy/maxProfit/MaxProfit1.java
@@ -0,0 +1,30 @@
+package easy.maxProfit;
+
+public class MaxProfit1 {
+
+    public static int maxProfit(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int temp = prices[i];
+            if (min > temp) {
+                min = temp;
+                continue;
+            }
+            if (temp - min > profit) {
+                profit = temp - min;
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3}));
+        System.out.println(maxProfit(new int[]{7, 2, 4, 8, 1}));
+    }
+
+}
